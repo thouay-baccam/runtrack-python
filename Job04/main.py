552ccbcc511069e7b95119a89948145f3492c96e
Teddy Baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-11-20 10:58:15 +0100

    Ajout jusqu'au job 07

---
+++ Job04/main.py
@@ -0,0 +1,10 @@
+import string  
+
+lower = list(string.ascii_lowercase)  
+print(lower)  
+    
+upper = list(string.ascii_uppercase)  
+print(upper)  
+    
+alphabets = list(string.ascii_letters)  
+print(alphabets)  
---
+++ Job05/main.py
@@ -0,0 +1,10 @@
+import string  
+
+lower = list(reversed(string.ascii_lowercase))
+print("Reversed lowercase:", lower)
+
+upper = list(reversed(string.ascii_uppercase))
+print("Reversed uppercase:", upper)
+
+alphabets = list(reversed(string.ascii_letters))
+print("Reversed combined:", alphabets)
