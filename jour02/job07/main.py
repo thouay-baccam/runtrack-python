2c071812d69f0d22ac672e13140c83a898e2d5ca
Teddy Baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-11-21 16:18:03 +0100

    fix

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -1,10 +1,13 @@
-# Chaîne de caractères initiale
-chaine = "abcdefghijklmnopqrstuvwxyz"
+# Définition de la chaîne en multipliant "abcdefghijklmnopqrstuvwxyz" par 10
+chaine = "abcdefghijklmnopqrstuvwxyz" * 10
 
-# Afficher une suite pyramidale de caractères
-n = 26  # Nombre de lignes de la pyramide
-for i in range(1, n + 1):
-    espace = " " * (n - i)  # Ajouter des espaces pour aligner à droite
-    motif = chaine[:i]  # Sélectionner les caractères de la chaîne
-    ligne = espace + motif + motif[-2::-1]  # Construire la ligne de la pyramide
-    print(ligne)
+# Définition de la variable n avec la valeur 40
+n = 40
+
+# Boucle for qui itère sur les valeurs de i de 1 à 39
+for i in range(1, n):
+    # Création d'un motif en extrayant les premiers 2*i - 2 caractères de la chaîne
+    motif = chaine[:2*i - 2]
+
+    # Affichage du motif actuel
+    print(motif)
