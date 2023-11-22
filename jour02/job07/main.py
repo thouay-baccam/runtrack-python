2f99287f5a3dc034dc65b6978b5610c568195658
Teddy Baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-11-22 08:01:11 +0100

    comme dans le job 7

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -4,10 +4,10 @@ chaine = "abcdefghijklmnopqrstuvwxyz" * 10
 # Définition de la variable n avec la valeur 40
 n = 40
 
-# Boucle for qui itère sur les valeurs de i de 1 à 39
-for i in range(1, n):
-    # Création d'un motif en extrayant les premiers 2*i - 2 caractères de la chaîne
-    motif = chaine[:2*i - 2]
+# Boucle for qui itère sur les valeurs de i de 1 à 29 avec un pas de 2
+for i in range(1, 30, 2):
+    # Création d'un motif en extrayant les premiers i caractères de la chaîne
+    motif = chaine[:i]
 
     # Affichage du motif actuel
     print(motif)
